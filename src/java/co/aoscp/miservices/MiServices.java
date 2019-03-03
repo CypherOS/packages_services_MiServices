671d2afbd0bbbdf5b69f24c1bcfbf6f261ea8cf9
Chris Crump <chris@cypheros.co>
2019-03-03 11:14:15 -0500

    MiServices: Import Weather Provider
    Moved from it's own repo to MiServices and adapted to support multiple queries without touching framework
    
    Change-Id: I0ffdb2f79c08df5ce6e67d828ba47346632a033c

---
+++ src/java/co/aoscp/miservices/MiServices.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 CypherOS
+ *
+ * MiServices is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MiServices is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MiServices.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package co.aoscp.miservices;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import co.aoscp.miservices.weather.WeatherController;
+
+public class MiServices extends BroadcastReceiver {
+
+    private static final String TAG = "MiServices";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Firing up aoscp addons");
+		WeatherController.get(context, true);
+    }
+}
