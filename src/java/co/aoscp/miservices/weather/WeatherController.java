420ec3532bbb487783634bd60118808e238583ab
Chris Crump <chris@cypheros.co>
2019-03-08 11:32:36 -0500

    MiServices: Introduce Quickspace
    Change-Id: I0bdeb1b743c3f019588523413ac1e97f1d3550a1

--- src/java/co/aoscp/miservices/weather/WeatherController.java
+++ src/java/co/aoscp/miservices/weather/WeatherController.java
@@ -16,6 +16,8 @@
  */
 package co.aoscp.miservices.weather;
 
+import static co.aoscp.miservices.weather.utils.Constants.DEBUG;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -26,83 +28,42 @@ import android.net.Uri;
 import android.os.Process;
 import android.util.Log;
 
+import co.aoscp.miservices.Bits;
+import co.aoscp.miservices.onetime.IControllers;
+import co.aoscp.miservices.quickspace.QuickspaceCard;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-public class WeatherController {
+public class WeatherController implements IControllers {
 
     private static final String TAG = "WeatherController";
-    private static final boolean DEBUG = false;
 
     private static final int WEATHER_UPDATE_INTERVAL = 60 * 10 * 1000; // 10 minutes
 
     private Context mContext;
     private static WeatherController sController;
 
-    private String mUpdateIntent;
     private PendingIntent mPendingUpdate;
-    private boolean mBootAndUnlocked;
     private boolean mIsRunning;
     private boolean mIsScreenOn = true;
 
-    private List<UpdateListener> mListeners;
+    private UpdateListener mListener;
     private long mLastUpdated;
     private long mScheduledAlarm = 0;
-    private int mUpdateStatus = WeatherProvider.WEATHER_UPDATE_ERROR;
+    private int mUpdateStatus = QuickspaceCard.WEATHER_UPDATE_ERROR;
     private AlarmManager mAlarmManager;
-
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent == null) {
-                return;
-            }
-            if (DEBUG) Log.d(TAG, "Received intent: " + intent.getAction());
-            if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
-                onScreenOff();
-            } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
-                onScreenOn();
-            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-                mBootAndUnlocked = true;
-                postUpdate(false);
-            } else if (mUpdateIntent.equals(intent.getAction())) {
-                postUpdate(false);
-            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction())
-                    || Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
-                postUpdate(true);
-            }
-        }
-    };
 
     public interface UpdateListener {
         void onPostUpdate();
     }
 
-    public static WeatherController get(Context context, boolean withReceiver) {
-        if (sController == null) {
-            sController = new WeatherController(context.getApplicationContext(), withReceiver);
-        }
-        return sController;
-    }
-
-    private WeatherController(Context context, boolean withReceiver) {
+    public WeatherController(Context context) {
         mContext = context;
-        if (withReceiver) {
-            mUpdateIntent = "updateIntent_" + Integer.toString(getRandomInt());
-            mPendingUpdate = PendingIntent.getBroadcast(mContext, getRandomInt(), new Intent(mUpdateIntent), 0);
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(Intent.ACTION_SCREEN_OFF);
-            filter.addAction(Intent.ACTION_SCREEN_ON);
-            filter.addAction(Intent.ACTION_BOOT_COMPLETED);
-            filter.addAction(Intent.ACTION_TIME_CHANGED);
-            filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-            filter.addAction(mUpdateIntent);
-            filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
-            mContext.registerReceiver(mReceiver, filter);
-        }
+        mPendingUpdate = PendingIntent.getBroadcast(mContext, getRandomInt(), new Intent(Bits.QUICKSPACE_UPDATE_ACTION), 0);
     }
 
     private int getRandomInt() {
@@ -110,8 +71,8 @@ public class WeatherController {
         return r.nextInt((20000000 - 10000000) + 1) + 10000000;
     }
 
-    private void postUpdate(boolean reset) {
-        if (!mBootAndUnlocked) return;
+    @Override
+    public void onUpdate(boolean reset) {
         if (mIsRunning) {
             if (reset) resetSchedule();
             return;
@@ -120,44 +81,49 @@ public class WeatherController {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (UpdateListener listener : mListeners) {
+                if (mListener != null) {
                     try {
-                        listener.onPostUpdate();
+                        mListener.onPostUpdate();
                     } catch (Exception ignored) {
                     }
                 }
                 mLastUpdated = System.currentTimeMillis();
+				mIsRunning = false;
                 resetSchedule();
             }
         });
         thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
-        mIsRunning = false;
     }
 
     private boolean needsUpdate() {
         boolean expired = System.currentTimeMillis() - mLastUpdated > WEATHER_UPDATE_INTERVAL;
-        return mUpdateStatus != WeatherProvider.WEATHER_UPDATE_SUCCESS || expired;
+        return expired;
     }
 
-    private void onScreenOn() {
-        if (!mBootAndUnlocked || mIsScreenOn){
+    @Override
+    public void onScreenOn() {
+        if (mIsScreenOn) {
+			if (DEBUG) Log.d(TAG, "Either the boot isn't complete or screen is already on");
             return;
         }
         if (DEBUG) Log.d(TAG, "onScreenOn");
         mIsScreenOn = true;
         if (!mIsRunning) {
             if (needsUpdate()) {
-                if (DEBUG) Log.d(TAG, "Needs update, triggering postUpdate");
-                postUpdate(false);
+                if (DEBUG) Log.d(TAG, "Needs update, triggering onUpdate");
+                onUpdate(false);
             } else {
                 if (DEBUG) Log.d(TAG, "Scheduling update");
                 scheduleUpdate();
             }
-        }
+        } else {
+			if (DEBUG) Log.d(TAG, "Update thread is still running");
+		}
     }
 
-    private void onScreenOff() {
+    @Override
+    public void onScreenOff() {
         if (DEBUG) Log.d(TAG, "onScreenOff");
         mIsScreenOn = false;
         cancelUpdate();
@@ -188,14 +154,10 @@ public class WeatherController {
     }
 
     public void setUpdateStatus(int status) {
-        mUpdateStatus = status;
+		mUpdateStatus = status;
     }
 
-    public void addUpdateListener(final UpdateListener listener) {
-        mListeners.add(listener);
-    }
-
-    public void removeUpdateListener(UpdateListener listener) {
-        mListeners.remove(listener);
+    public void addUpdateListener(UpdateListener listener) {
+        mListener = listener;
     }
 }
